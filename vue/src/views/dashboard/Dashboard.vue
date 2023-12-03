228ac7ebfb58576dcf5b8a4868fc85596f9a256b
Anthony <blacknebula236@gmail.com>
2023-12-03 22:40:56 +1030

    edited text

--- vue/src/views/dashboard/Dashboard.vue
+++ vue/src/views/dashboard/Dashboard.vue
@@ -4,7 +4,7 @@
     <Container>
         <div class="mx-5 text-center">
             <h3>Welcome to Story Share</h3>
-            <p>A fan-run place to create & share stories. The goal of this place is for aspiring storytellers to sharpen & grow their skills, whether it be for writing a novel, a children's picture book, a comic or even a TTRPG campaign. </p>
+            <p>A place to create & share stories. The goal of this place is for aspiring storytellers to sharpen & grow their skills, whether it be for writing a novel, a children's picture book, a comic or even a TTRPG campaign. </p>
         </div>
     </Container>
   </template>
