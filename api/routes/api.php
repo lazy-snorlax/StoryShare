25ac5b66ed5f269aa2feaccfc320a281ba61b0be
Anthony <blacknebula236@gmail.com>
2023-12-03 22:35:14 +1030

    writing API for login/logout routes. adding login user  resources.

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,33 +1,25 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', fn () => null);
+
+// Login/Logout =======================================================
+Route::post('/login', [Controllers\LoginController::class, 'store']);
+Route::post('/logout', [Controllers\LoginController::class, 'destroy']);
+
+// TODO: Email Verification =================================================
+
+
+// TODO: Password Reset =====================================================
+
+
+// TODO: Global Lookups =====================================================
+
+
+// Authenticated ============================================================
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', Controllers\LoggedInController::class);
 });
-
-Route::post('/register', [RegisteredUserController::class, 'store'])
-                ->middleware('guest')
-                ->name('register');
-
-Route::post('/login', function (Request $request) {
-    dd($request);
-    return $request->user();
-});
-
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth')
-                ->name('logout');
-
