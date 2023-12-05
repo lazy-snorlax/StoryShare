1b63c3a368f9f89ecf0f1afc94a8d9869bac88b7
Anthony <blacknebula236@gmail.com>
2023-12-05 18:27:42 +1030

    add attribute to User model for encrypting passwords

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -41,4 +42,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    /**
+     * Password attribute caster.
+     */
+    public function password(): Attribute
+    {
+        return new Attribute(set: fn ($value) => $value ? bcrypt($value) : null);
+    }
 }
