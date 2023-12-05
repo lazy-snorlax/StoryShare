691bf2bbf7bca3e7001fccdefcf12aa6201a9258
Anthony <blacknebula236@gmail.com>
2023-12-05 11:17:14 +1030

    added updatePassword request validation

--- api/app/Http/Controllers/UpdatePasswordController.php
+++ api/app/Http/Controllers/UpdatePasswordController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use App\Http\Requests\LoggedInUser\UpdatePasswordRequest;
 
 class UpdatePasswordController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(UpdatePasswordRequest $request)
     {
         $user = $request->user();
 
