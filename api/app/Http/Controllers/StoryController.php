682be576e1e89c485ca35773f7d955aaf33c0511
Anthony <blacknebula236@gmail.com>
2023-12-12 20:33:18 +1030

    rewrote index() allowing for query variables

--- api/app/Http/Controllers/StoryController.php
+++ api/app/Http/Controllers/StoryController.php
@@ -14,10 +14,18 @@ class StoryController extends Controller
     public function index(Request $request)
     {
         $query = Story::with('user')
-            ->orderBy('updated_at', 'desc')
-            ->get();
+            ->when($request->query('search'), function($query, $search) {
+                $query->whereSearchByWords($search, [ 'title', 'summary' ]);
+            })
+            ->when($request->query('sort'), function ($query, $sort) {
+                if (str_starts_with($sort, '-')){
+                    $query->orderBy(substr($sort, 1), 'desc');
+                } else {
+                    $query->orderBy($sort, 'asc');
+                }
+            });
 
-        return StoryListResource::collection($query);
+        return StoryListResource::collection($query->get());
     }
 
     /**
