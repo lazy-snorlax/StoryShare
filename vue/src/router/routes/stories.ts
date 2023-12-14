d579693753511262b04d6cad7651e949f4d53895
Anthony <blacknebula236@gmail.com>
2023-12-14 23:53:50 +1030

    adding routes for chapters

--- vue/src/router/routes/stories.ts
+++ vue/src/router/routes/stories.ts
@@ -1,10 +1,11 @@
 import { routes } from '@/utilities/routes'
 import StorySearch from '../../views/stories/StorySearch.vue'
+import StorySingle from '../../views/stories/StorySingle.vue'
 
 export default routes(
     {
         template: 'app',
-        restricted: true
+        restricted: false
     },
     [
         {
@@ -19,7 +20,23 @@ export default routes(
                     path: '/stories/search',
                     name: 'stories.search',
                     component: StorySearch
-                }
+                },
+                {
+                    path: '/story/:id',
+                    name: 'story.single',
+                    component: StorySingle,
+                    children:
+                    [
+                        {
+                            path: '/stories/:id/chapter/all',
+                            name: 'story.chapter.all',
+                        },
+                        {
+                            path: '/stories/:id/chapter/:chapter',
+                            name: 'story.chapter.single',
+                        },
+                    ]
+                },
             ]
         }
     ]
