6458c69a4ed0b267e76a7c60ac04d0349ea6079a
Anthony <blacknebula236@gmail.com>
2023-12-14 23:53:15 +1030

    adding types & api call to store

--- vue/src/stores/story.ts
+++ vue/src/stores/story.ts
@@ -1,5 +1,4 @@
 import { defineStore } from "pinia";
-import { MyStoryListResource } from "./my-story";
 
 export const useStoryStore = defineStore('story', {
     state: (): StoryState => ({
@@ -10,13 +9,41 @@ export const useStoryStore = defineStore('story', {
         async getStoryList(filters: URLSearchParams | null) {
             const response = await this.http.get('stories' + (filters ? '?' + filters.toString() : ''))
             this.list = response.data.data
+        },
+
+        async getStory(id: number) {
+            const response = await this.http.get('stories/' + id)
+            this.story = response.data.data
         }
     },
 })
 
 type StoryState = {
-    story: StoryListResource,
+    story: StoryResource,
     list: Array<StoryListResource>
+}
+
+export type StoryResource = {
+    id: number,
+    user: string,
+    title: string,
+    summary: string,
+    notes: string,
+    number_of_chapters: number,
+    word_count: number,
+    posted: boolean,
+    complete: boolean,
+    created_at: string,
+    updated_at: string,
+    chapters: Array<ChapterListResource> | null,
+}
+
+export type ChapterListResource = {
+    id: number,
+    chapter_number: number,
+    title: string,
+    summary: string,
+    updated_at: string,
 }
 
 export type StoryListResource = {
