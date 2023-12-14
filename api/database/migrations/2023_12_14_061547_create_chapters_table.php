183d4f9a1f0a363f1064da2a1f445be7c223ab6c
Anthony <blacknebula236@gmail.com>
2023-12-14 23:30:02 +1030

    create migrations for chapters, tags & comments

---
+++ api/database/migrations/2023_12_14_061547_create_chapters_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->id();
+            $table->integer('story_id');
+            $table->integer('chapter_number');
+            $table->string('title');
+            $table->text('summary')->nullable();
+            $table->mediumText('content')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chapters');
+    }
+};
