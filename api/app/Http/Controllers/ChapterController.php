ee03aab2db3624c84724f5bc5fca02e5aea8cc1f
Anthony <blacknebula236@gmail.com>
2023-12-14 23:34:01 +1030

    created chapter controller

---
+++ api/app/Http/Controllers/ChapterController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chapter;
+use Illuminate\Http\Request;
+
+class ChapterController extends Controller
+{
+    /**
+     * List all chapters of a story
+     */
+    public function index(Request $request, )
+    {
+        
+    }
+
+    /**
+     * Show a single chapter 
+     */
+    public function show(Chapter $chapter)
+    {
+        //
+    }
+
+    // *******************************************************************
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chapter $chapter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chapter $chapter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chapter $chapter)
+    {
+        //
+    }
+}
