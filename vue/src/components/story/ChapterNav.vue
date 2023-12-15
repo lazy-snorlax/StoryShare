34374b4c7a0ad9b085fe68039d568384dba89e1b
Anthony <blacknebula236@gmail.com>
2023-12-15 22:19:03 +1030

    story/chapter nav controls

---
+++ vue/src/components/story/ChapterNav.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="row my-3 d-flex">
+        <!-- <div class="col mx-auto text-center">
+            <a class="chapter-btn btn">
+                View All Chapters
+            </a>
+        </div> -->
+        <div class="col mx-auto text-center" v-if="props.chapter_number > 1">
+            <router-link class="chapter-btn btn" :to="{ name: 'story.chapter.single', params: { chapter: props.chapter_number-1 } }">
+                Previous Chapter
+            </router-link>
+        </div>
+        <div class="col mx-auto text-center">
+            <a class="chapter-btn btn">
+                Jump to Chapter
+            </a>
+        </div>
+        <div class="col mx-auto text-center" v-if="props.chapter_number !== props.number_of_chapters">
+            <router-link class="chapter-btn btn" :to="{ name: 'story.chapter.single', params: { chapter: props.chapter_number+1 } }">
+                Next Chapter
+            </router-link>
+        </div>
+        <!-- <div class="col mx-auto text-center">
+            <a class="chapter-btn btn">
+                Comments
+            </a>
+        </div>
+        <div class="col mx-auto text-center">
+            <a class="chapter-btn btn">
+                Share
+            </a>
+        </div>
+        <div class="col mx-auto text-center">
+            <a class="chapter-btn btn">
+                Download
+            </a>
+        </div> -->
+    </div>
+</template>
+<script lang="ts" setup>
+
+const props = defineProps({
+    chapter_number: Number,
+    number_of_chapters: Number
+})
+
+</script>
--- vue/src/views/stories/StoryChapter.vue
+++ vue/src/views/stories/StoryChapter.vue
@@ -2,19 +2,23 @@
 
     <Header :title="chapter?.title" />
 
-    <Container class="story-content">
-        <div class="mb-3" v-if="chapter?.summary">
-            <h3>Summary</h3>
-            <p>{{ chapter?.summary }}</p>
-        </div>    
-            
-        <div class="my-5 content">
-            {{ chapter?.content }}
-        </div>
+    <Container class="">
+        <ChapterNav class="chapter-nav" :chapter_number="chapter?.chapter_number" :number_of_chapters="chapter?.story?.number_of_chapters" />
 
-        <div class="mt-3" v-if="chapter?.notes">
-            <h3>Notes</h3>
-            <p>{{ chapter.notes }}</p>
+        <div class="story-content">
+            <div class="mb-3" v-if="chapter?.summary">
+                <h3>Summary</h3>
+                <p>{{ chapter?.summary }}</p>
+            </div>    
+            
+            <div class="my-5 content">
+                {{ chapter?.content }}
+            </div>
+            
+            <div class="mt-3" v-if="chapter?.notes">
+                <h3>Notes</h3>
+                <p>{{ chapter.notes }}</p>
+            </div>
         </div>
     </Container>
 
@@ -23,6 +27,7 @@
 import { useChapter } from '../../composables/stories/use-get-chapter';
 import { useRoute } from 'vue-router';
 import { onMounted } from 'vue';
+import ChapterNav from '../../components/story/ChapterNav.vue';
 
 const { chapter, getChapters } = useChapter()
 const route = useRoute()
