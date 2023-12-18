905c769b4de182fd7fd9f5b55373af900eb81dbb
Anthony <blacknebula236@gmail.com>
2023-12-18 23:32:57 +1030

    my chapter list item with correct vue route

---
+++ vue/src/components/app/my-story/my-chapters/MyChapterListItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <router-link :to="{ name: 'my-stories.chapter.single', params: { chapter: chapter.id } }" class="chapter-item">
+        <li>
+            <h5>{{ chapter.title }}</h5>
+            <p>{{ chapter.summary }}</p>
+            <small>Updated: {{ chapter.updated_at }}</small>
+        </li>
+    </router-link>
+</template>
+<script lang="ts" setup>
+
+const props = defineProps({
+    chapter: {
+        id: Number,
+        chapter_number: Number,
+        title: String,
+        summary: String,
+        updated_at: String
+    }
+})
+
+</script>
