9d17b73ca59699f09ce72843cbc82339d818446f
Anthony <blacknebula236@gmail.com>
2023-12-18 13:16:48 +1030

    added vue-final-modal to project

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -16,16 +16,22 @@ import 'bootstrap'
 import 'vue-multiselect/dist/vue-multiselect.css'
 import MultiSelect from 'vue-multiselect'
 
+import 'vue-final-modal/style.css'
+import { createVfm } from 'vue-final-modal'
+const vfm = createVfm()
+
 const app = createApp(App)
 
 app.use(http, { router })
 app.use(pinia)
 app.use(router)
 app.use(components)
+app.use(vfm)
 
 // Font Awesome Icons
 app.component('font-awesome-icon', FontAwesomeIcon)
 
+// Utility Components
 app.component('multi-select', MultiSelect)
 
 app.mount('#app')
