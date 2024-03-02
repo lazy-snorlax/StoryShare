b72e9e0d14305c31e23572ae8e3e3096f23fc1c7
Anthony <blacknebula236@gmail.com>
2024-03-02 22:47:11 +1030

    adding bouncer facade to testcase

--- api/tests/TestCase.php
+++ api/tests/TestCase.php
@@ -2,9 +2,75 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Silber\Bouncer\BouncerFacade;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Setup test case
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $this->setUpParallel();
+
+        parent::setUp();
+
+        $this->withoutMiddleware([ThrottleRequests::class]);
+        $this->withHeaders(['referer' => env('APP_URL')]);
+
+        // If method doesn't exist automatically verify two factor, otherwise check result of
+        // method to determine if it should be verified or not.
+        // phpcs:ignore
+        if (!method_exists($this, 'shouldTwoFactorBeVerified') || $this->shouldTwoFactorBeVerified()) {
+            $this->withTwoFactorVerified();
+        }
+
+        // Reset model results per page to 50.
+        // Model::resultsPerPage(50);
+
+        BouncerFacade::dontCache();
+
+        if (in_array(RefreshDatabase::class, class_uses_recursive($this))) {
+            Artisan::call('abilities:refresh');
+        }
+
+        Storage::fake();
+    }
+
+    /**
+     * Setup tests to run in parallel.
+     *
+     * @return void
+     */
+    protected function setUpParallel()
+    {
+        if (($parallel = getenv('TEST_TOKEN')) !== false) {
+            putenv("DB_TESTING_DATABASE=testing_{$parallel}");
+        }
+    }
+
+    /**
+     * Create a subscription management user.
+     */
+    public function createUser(array $attributes = []): User
+    {
+        return User::factory()->user()->enabled()->create($attributes);
+    }
+
+    /**
+     * Create a subscription management admin.
+     */
+    public function createAdmin(array $attributes = []): User
+    {
+        return User::factory()->admin()->enabled()->create($attributes);
+    }
 }
