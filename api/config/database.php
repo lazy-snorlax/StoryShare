473622001bca963e33aa960e15d937b679be84d7
Anthony <blacknebula236@gmail.com>
2024-03-02 22:46:39 +1030

    setting up testing db for unit & feature tests

--- api/config/database.php
+++ api/config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'testing' => [
+            'driver' => 'mysql',
+            'url' => env('TESTING_DATABASE_URL'),
+            'host' => env('DB_TESTING_HOST', env('DB_HOST')),
+            'port' => env('DB_TESTING_PORT', env('DB_PORT')),
+            'database' => env('DB_TESTING_DATABASE', (getenv('TEST_TOKEN') !== false ? 'testing_'.getenv('TEST_TOKEN') : 'qa_staging_cba')),
+            'username' => env('DB_TESTING_USER', env('DB_USERNAME')),
+            'password' => env('DB_TESTING_PASSWORD', env('DB_PASSWORD')),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
