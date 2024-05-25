5ad6dbcc82c4daeb121fe0dcca49e14205e1b2cc
Anthony <blacknebula236@gmail.com>
2024-05-26 03:24:47 +0930

    function calls for emitted pagination controls

--- vue/src/views/stories/StorySearch.vue
+++ vue/src/views/stories/StorySearch.vue
@@ -5,17 +5,17 @@
         <Container class="m-auto p-4 d-flex flex-column" style="width: -moz-available;">            
             
             <div class="d-inline-flex w-100">
-                <StorySearchSubNav @toggleFilter="toggleFilters" @triggerReload="reloadStoryList" />
+                <StorySearchSubNav :pagination="results.pagination" @toggleFilter="toggleFilters" @triggerReload="reloadStoryList" @pagePrevious="previous" @pageNext="next" @pageJump="pageJump"/>
             </div>
 
             <div class="d-inline-flex">
                 <div class="story-list w-100">
-                    <template v-for="item in list">
+                    <template v-for="item in results.list">
                         <StoryListItem :item="item" />
                     </template>
-                </div>    
+                </div>
                 <!-- Story Search filters -->
-                <StorySearchFilters :filters="filters" :resultNumber="resultNumber" />
+                <StorySearchFilters :filters="filters" :resultNumber="results.pagination.total" />
             </div>
 
         </Container>
@@ -37,17 +37,13 @@ import { storeToRefs } from 'pinia'
 import { useAuthStore } from '@/stores/auth'
 
 const { getStoryList } = useStoryStore()
-const { list } = useStoryList()
+const { results } = useStoryList()
 const filters = ref({
     search: null,
     author: null,
     sort: 'updated_at',
     genre: [],
     tags: [],
-})
-
-const resultNumber = computed(() => {
-    return list.value.length
 })
 
 onMounted(async () => {
@@ -64,6 +60,18 @@ watch(
 
 const reloadStoryList = async () => {
     await getStoryList(filters.value)
+}
+
+const pageJump = async (page = 1) => {
+    await getStoryList(filters.value, page)
+}
+
+const next = async () => {
+    await getStoryList(filters.value, results.value.pagination?.current_page < results.value.pagination?.last_page ? results.value.pagination?.current_page + 1 : results.value.pagination?.current_page)
+}
+
+const previous = async () => {
+    await getStoryList(filters.value, results.value.pagination.current_page > 1 ? results.value.pagination.current_page - 1 : 1)
 }
 
 const { toggleFilters } = useAuthStore()
