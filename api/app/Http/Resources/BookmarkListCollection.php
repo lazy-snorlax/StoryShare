0024d32df86e36c28ec34f19756c7e513a329be9
Anthony <blacknebula236@gmail.com>
2024-06-02 17:38:26 +0930

    format bookmark list return

---
+++ api/app/Http/Resources/BookmarkListCollection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class BookmarkListCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request)
+    {
+        return [
+            'data' => $this->collection->transform(function ($list) {
+                return new BookmarkListResource($list);
+            }),
+        ];
+    }
+}
---
+++ api/app/Http/Resources/BookmarkListResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookmarkListResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'private' => $this->private,
+            'notes' => $this->notes,
+            'created_at' => $this->created_at?->format('d M Y'),
+            'updated_at' => $this->updated_at?->format('d M Y'),
+            'story' => new StoryResource($this->story->load('genres'))
+        ];
+    }
+}
