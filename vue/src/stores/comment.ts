840977720f293e762f2a836aeceb8e8244126835
Anthony <blacknebula236@gmail.com>
2024-08-27 23:07:43 +0930

    add get comments to store

--- vue/src/stores/comment.ts
+++ vue/src/stores/comment.ts
@@ -2,12 +2,13 @@ import { defineStore } from "pinia"
 
 export const useCommentsStore = defineStore('comments', {
     actions: {
+        async getComments(parent_id) {
+            const response = await this.http.get('comments/?parent_id=' + parent_id)
+            return response.data
+        },
         async commentChapter(comment) {
             const response = await this.http.post('comment', comment)
             return response.data
-            // .then(response => {
-            //     console.log('Successfully added comment')
-            // })
         },
         async deleteComment(comment_id) {
             const response  = await this.http.delete('comment/' + comment_id)
