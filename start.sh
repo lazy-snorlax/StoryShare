401839e453c8bdb3a5f3170a17e19953530c3461
Anthony <blacknebula236@gmail.com>
2024-09-01 23:11:53 +0930

    tweak docker cmd

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ docker compose -f docker-compose-build.yml exec build-vue npm run staging
 docker compose -f docker-compose-build.yml stop
 
 docker compose -f docker-compose-prod.yml build
-docker compose -f docker-compose-prod.yml up
+docker compose -f docker-compose-prod.yml up -d
