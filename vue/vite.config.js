8652e22c128aaad8813120fd92404b17d5520b47
Anthony <blacknebula236@gmail.com>
2024-09-01 22:42:16 +0930

    prod nginx & vite configs

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -5,13 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 8080
-  },
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: true,
+    port: 8080
   }
 })
