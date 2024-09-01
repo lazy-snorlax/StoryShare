e3e3f79becc6f98f21246ebddb9a6d07d1bc7106
Anthony <blacknebula236@gmail.com>
2024-09-01 22:40:53 +0930

    created docker build & prod yml files + prod Dockerfiles + env files

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+docker compose -f docker-compose-build.yml up -d --build
+docker compose -f docker-compose-build.yml exec build-fpm composer install --no-dev
+docker compose -f docker-compose-build.yml exec build-vue npm run staging
+docker compose -f docker-compose-build.yml stop
+
+docker compose -f docker-compose-prod.yml build
+docker compose -f docker-compose-prod.yml up
