5847aa2a59df166e990e786635ea9d985dbdbed0
Anthony <blacknebula236@gmail.com>
2024-09-11 21:36:01 +0930

    total comment count

--- api/app/Http/Resources/ChapterListResource.php
+++ api/app/Http/Resources/ChapterListResource.php
@@ -20,7 +20,7 @@ class ChapterListResource extends JsonResource
             'title' => $this->title,
             'summary' => $this->summary,
             'word_count' => $this->word_count,
-            'comments' => $this->whenCounted('comments'),
+            'comment_count' => $this->allComments()->count(),
             'updated_at' => $this->updated_at->format('d M Y'),
         ];
     }
