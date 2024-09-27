d465276ab38e27a8bc433abf1c6a635d42a981c1
Anthony <blacknebula236@gmail.com>
2024-09-27 21:23:59 +0930

    revert to webhashhistory

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import {routes as defineRoutes} from '@/utilities/routes'
 import routes from './routes'
 import authenticated from './guards/authenticated'
 import authorized from './guards/authorized'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     ...routes,
     // ...defineRoutes(
@@ -27,6 +27,8 @@ const router = createRouter({
 
 // Bind the authenticated and authorized guards to ensure users are logged in and have the correct permission.
 authenticated(router)
+
+/** Check the abilites of the user */
 // authorized(router)
 
 // Bind the other middleware to handle things such as account verification and two factor.
