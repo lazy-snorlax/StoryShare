080635b6a2e603c743e7ca3cc124c0f40ef6b977
Anthony <blacknebula236@gmail.com>
2024-10-06 23:17:39 +1030

    rating controller & resource

---
+++ api/app/Http/Controllers/RatingController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\RatingListResource;
+use App\Models\Rating;
+use Illuminate\Http\Request;
+
+class RatingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $query = Rating::query()->get();
+
+        return RatingListResource::collection($query);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Rating $rating)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Rating $rating)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Rating $rating)
+    {
+        //
+    }
+}
