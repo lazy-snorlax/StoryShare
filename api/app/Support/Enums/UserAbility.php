824d2ef7f25958306eed1629b5fa292126dcfbd6
Anthony <blacknebula236@gmail.com>
2024-12-31 14:39:10 +1030

    create more user abilities as needed

--- api/app/Support/Enums/UserAbility.php
+++ api/app/Support/Enums/UserAbility.php
@@ -6,13 +6,25 @@ enum UserAbility: string
 {
     case ReadUsers = 'read users';
     case WriteUsers = 'write users';
+    case ReadBan = 'read ban';
+    case WriteBan = 'write ban';
+    case ReadComment = 'read comment';
+    case WriteComment = 'write comment';
 
     public static function grouped(): array
     {
         return [
             'users' => [
-                'read' => ['name' => self::ReadUsers, 'excludes' => []],
-                'write' => ['name' => self::WriteUsers, 'excludes' => []],
+                'read' => ['name' => self::ReadUsers, 'excludes' => [RoleType::User]],
+                'write' => ['name' => self::WriteUsers, 'excludes' => [RoleType::User]],
+            ],
+            'ban-users' => [
+                'read' => ['name' => self::ReadBan, 'excludes' => [RoleType::User]],
+                'write' => ['name' => self::WriteBan, 'excludes' => [RoleType::User]],
+            ],
+            'comment' => [
+                'read' => ['name' => self::ReadComment, 'excludes' => []],
+                'write' => ['name' => self::WriteComment, 'excludes' => []],
             ],
         ];
     }
