6c4f6388cdd9f7a8729d62a21e67237030064f6a
Anthony <blacknebula236@gmail.com>
2025-01-02 17:41:21 +1030

    add auth tests

--- api/tests/Feature/Auth/AuthenticationTest.php
+++ api/tests/Feature/Auth/AuthenticationTest.php
@@ -4,7 +4,11 @@ namespace Tests\Feature\Auth;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
+
+use function PHPUnit\Framework\isNull;
 
 class AuthenticationTest extends TestCase
 {
@@ -13,15 +17,54 @@ class AuthenticationTest extends TestCase
     public function testCanLoginWithCorrectEmailAndPassword()
     {
         $this->createUser(['email' => 'john@email.com']);
-
         $response = $this->postJson('api/login', [
             'email' => 'john@email.com', 
-            'password' => 'Secret*12345'
+            'password' => 'Secret*12345',
         ]);
 
-        // dd($response);
-
         $response->assertSuccessful();
-        // $response->assertSessionHas(RegistrationSession::Registration->value, false);
+    }
+
+    // public function testCanSeeUserIsLockedOutAfterTooManyLoginAttemptsWithIncorrectDetails() {
+    //     $this->createAdmin(['email' => 'admin@email.com']);
+
+    //     $attempts = config('auth.limiters.login.max_attempts');
+
+    //     while ($attempts > 0) {
+    //         $this->postJson('login', ['email' => 'john@email.com', 'password' => 'Incorrect*12345'])->assertStatus(422);
+    //         $attempts--;
+    //     }
+
+    //     // 6th time is with correct information but denied due to already being locked out.
+    //     $response = $this->postJson('login', [
+    //         'email' => 'john@email.com', 
+    //         'password' => 'Secret*12345'
+    //     ]);
+    //     $response->assertStatus(Response::HTTP_TOO_MANY_REQUESTS);
+    //     $response->assertJsonFragment([
+    //         'email' => [
+    //         'Due to 5 failed login attempts your IP and email address has been locked out for 30 minutes'
+    //     ]]);
+    // }
+
+    public function testCanGetLoggedInUser() {        
+        $response = $this->be($user = $this->createAdmin())->getJson('api/user');
+        $response->assertSuccessful();
+        
+        $response->assertJson(fn (AssertableJson $json) => $json
+            ->has('data', fn (AssertableJson $json) => $json
+                ->where('id', $user->id)
+                ->where('name', $user->name)
+                ->where('email', $user->email)
+                ->where('email_verified', !!isNull($user->email_verified_at))
+                ->has('abilities')
+                ->has('role', fn (AssertableJson $json) => $json
+                    ->where('id', $user->role->id)
+                    ->where('name', $user->role->name)
+                    ->where('title', $user->role->title)
+                    ->where('type', $user->role->type)
+                    ->has('abilities')
+                )
+        ));
     }
 }
