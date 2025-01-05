ba4d3315a45a30c8bf77b938f0c18d2f8b559ada
Anthony <blacknebula236@gmail.com>
2025-01-05 21:10:16 +1030

    move story access test into own file

--- api/tests/Feature/SingleStoryTest.php
+++ api/tests/Feature/SingleStoryTest.php
@@ -4,12 +4,9 @@ namespace Tests\Unit\Models;
 
 use App\Models\Story;
 use App\Models\User;
-use Database\Seeders\RatingSeeder;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Testing\Fluent\AssertableJson;
-
-use function PHPUnit\Framework\isNull;
 
 class SingleStoryTest extends TestCase
 {
@@ -27,37 +24,5 @@ class SingleStoryTest extends TestCase
         ]);
 
         $response->assertSuccessful();
-    }
-    
-    public function testGuestsCanAccessAPublicStory() {
-        $user = User::factory()->user()->create();
-        $userStory = Story::factory()->create([
-            'user_id' => $user->id,
-            'visible' => 'public',
-            'posted' => false
-        ]);
-        
-        $response = $this->getJson('api/stories/' . $userStory->id);
-        $response->assertSuccessful();
-        $response->assertJson(fn (AssertableJson $json) => $json
-            ->has('data', fn (AssertableJson $json) => $json
-                ->where('id', $userStory->id)
-                ->where('title', $userStory->title)
-                ->where('summary', $userStory->summary)
-                ->where('notes', $userStory->notes)
-                ->where('number_of_chapters', $userStory->number_of_chapters)
-                ->has('posted')
-                ->has('word_count')
-                ->has('complete')
-                ->where('visible', $userStory->visible)
-                ->where('created_at', $userStory->created_at->format('d M Y'))
-                ->where('updated_at', $userStory->updated_at->format('d M Y'))
-                ->where('bookmark', $userStory->bookmark)
-                ->where('applauded', $userStory->applauded)
-                ->has('applause')
-                ->has('rating')
-                ->has('genres')
-                ->has('chapters')
-            ));
     }
 }
