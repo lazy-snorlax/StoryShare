a086c862ba83ce7467513fdbe31cb515aed68fbc
Anthony <blacknebula236@gmail.com>
2025-01-14 21:32:13 +1030

    add security tests

--- api/tests/Unit/CommentTest.php
+++ api/tests/Unit/CommentTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Models;
 
 use App\Models\Chapter;
+use App\Models\Comment;
 use App\Models\Story;
 use App\Models\User;
 use Database\Factories\ChapterFactory;
@@ -34,7 +35,6 @@ class CommentTest extends TestCase
             'content' => 'Test Comment',
             'parent_id' => $chapter->id,
         ]);
-
         $response->assertSuccessful();
         $response->assertJson(fn (AssertableJson $json) => $json
             ->has('data', fn (AssertableJson $json) => $json
@@ -83,5 +83,64 @@ class CommentTest extends TestCase
                 ->where('created_at', now()->format('H:i D, d M Y'))
                 ->where('updated_at', now()->format('H:i D, d M Y'))
         ));
+    }
+
+    public function testAUserCannotEditSomeoneElsesComment() {
+        $user = $this->createUser();
+        $story = Story::factory()->create([
+            'user_id' => $user->id
+        ]);
+        $chapter = Chapter::factory()->create([
+            'story_id' => $story->id,
+            'chapter_number' => 1,
+            'title' => 'Chapter ' . 1,
+            'summary' => 'This is chapter ' . 1 . ' in story ' . $story->title,
+            'notes' => 'This is a section for any author notes on this chapter.',
+        ]);
+
+        $comment = Comment::factory()->create([
+            'user_id' => $user->id,
+            'parent_id' => $chapter->id,
+            'parent_type' => 'comment',
+
+            'commentable_id' => $chapter->id,
+            'commentable_type' => Chapter::class,
+            'content' => 'This is a test comment',
+
+            'approved' => true,
+            'approved_by' => 2,
+            'approved_at' => now(),
+        ]);
+
+        $testUser = $this->createUser();
+        $response = $this->be($testUser)->putJson('api/comment/' . $comment->id, [
+            'id' => $comment->id,
+            'content' => 'This is an edit test',
+        ]);
+        $response->assertStatus(401);
+        $response->assertJsonFragment(['message' => 'You are not authorized to do this.']);
+    }
+
+    public function testAGuestCannotCommentOnAStoryChapter() {
+        $user = $this->createUser();
+        $story = Story::factory()->create([
+            'user_id' => $user->id
+        ]);
+        $chapter = Chapter::factory()->create([
+            'story_id' => $story->id,
+            'chapter_number' => 1,
+            'title' => 'Chapter ' . 1,
+            'summary' => 'This is chapter ' . 1 . ' in story ' . $story->title,
+            'notes' => 'This is a section for any author notes on this chapter.',
+        ]);
+
+        $response = $this->postJson('api/comment', [
+            'chapter_id' => $chapter->id,
+            'content' => 'Test Comment',
+            'parent_id' => $chapter->id,
+        ]);
+
+        $response->assertStatus(401);
+        $response->assertJsonFragment(['message' => 'Unauthenticated.']);
     }
 }
