43255ae237562c0481159cf79d31aa5229c67b4a
Anthony <blacknebula236@gmail.com>
2025-01-14 21:31:07 +1030

    update error status number

--- api/app/Http/Controllers/CommentController.php
+++ api/app/Http/Controllers/CommentController.php
@@ -60,7 +60,7 @@ class CommentController extends Controller
      */
     public function update(Request $request, Comment $comment)
     {
-        abort_if($comment->user_id !== auth()->user()->id, 500, 'You are not authorized to do this.');
+        abort_if($comment->user_id !== auth()->user()->id, 401, 'You are not authorized to do this.');
         // dd($request->input(), $comment);
         $comment->fill($request->only('content'));
         $comment->save();
@@ -72,7 +72,7 @@ class CommentController extends Controller
      */
     public function destroy(Comment $comment)
     {
-        abort_if($comment->user_id !== auth()->user()->id, 500, 'You are not authorized to do this.');
+        abort_if($comment->user_id !== auth()->user()->id, 401, 'You are not authorized to do this.');
 
         // TODO: Delete replies of replies
         $comment->replies()->delete();
