5cc82e126656063f5248f289eb34e04e3141cd2f
Anthony <blacknebula236@gmail.com>
2025-01-20 23:48:55 +1030

    create profile store & implement into view

---
+++ vue/src/stores/profile.ts
@@ -0,0 +1,31 @@
+import { defineStore } from "pinia";
+import { StoryListResource } from "./story";
+
+export const useProfileStore = defineStore('profile', {
+    state: (): UserProfileState => ({
+        profile: null,
+    }),
+    actions: {
+        async getProfile(id: number) {
+            const response = await this.http.get('profile/' + id)
+            this.profile = response.data.data
+        },
+
+        async saveProfile(values: ProfileState, id) {
+            await this.http.put('profile/' + id, values)
+        },
+    }
+})
+
+type UserProfileState = {
+    profile: ProfileState | null,
+}
+
+export type ProfileState = {
+    name: string,
+    joined: string,
+    about_me: string | null,
+    language: string,
+    recent_stories: Array<StoryListResource>,
+    recent_bookmarks: Array<StoryListResource>,
+}
--- vue/src/views/Profile.vue
+++ vue/src/views/Profile.vue
@@ -1,35 +1,74 @@
 <template>
-    <Header :title="''" />
+    <Header />
 
-    <Container class="">
+    <Container class="user-profile">
 
-        <h1 class="text-center">Profile Name</h1>
         <div class="mb-3 profile">
             <div class="profile-pic">
                 <img :src="profile" alt="profile.jpg" class="">
             </div>
-            <div class="blurb">
-                <h2>Bio</h2>
-                <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Urna donec porttitor hendrerit tortor fames quisque. Primis scelerisque sem quam enim tortor id pretium dui. Euismod feugiat sollicitudin tempor, semper urna maecenas magna placerat. Ullamcorper nibh diam eleifend rutrum, mollis porta parturient. Sem fames odio nam aptent nec eu felis.</p>
-                <p>Vel imperdiet dapibus purus tristique maximus at. Ultrices pretium fermentum vivamus malesuada penatibus feugiat. Varius et taciti aliquam nostra blandit natoque dictumst bibendum ex. Euismod mus varius dignissim nec a; sagittis hendrerit praesent. Phasellus etiam nunc risus; vivamus in odio sollicitudin risus phasellus. Sociosqu mattis arcu taciti tellus consectetur duis facilisi. Augue commodo justo varius scelerisque nunc dolor velit. Leo nisi pellentesque mollis turpis fringilla.</p>
+            <div class="stats">
+                <h2 class="text-center">{{ profile?.name }}</h2>
+                <h5 class="">Joined: {{ profile?.joined }}</h5> 
+                <!-- <h5 class="">Subscribers: 100</h5> -->
+                <h5 class="">Language: {{ profile?.language }}</h5> 
             </div>
+        </div>
+
+        <div class="mb-3 bio">
+            <h2>Description</h2>
+            {{ profile?.about_me }}
         </div>
 
         <div class="recents">
             <div class="stories">
-                <h2>Recent Stories</h2>
+                <div class="header">
+                    <h2>Recent Stories</h2>
+                </div>
+                <div class="body">
+                    <template v-for="item in profile?.recent_stories">
+                        <StoryListItem :item="item" />
+                    </template>
+                </div>
+                <div class="footer mt-3 text-end"><a class="">See all stories >></a></div>
             </div>
+        </div>
+        <div class="recents">
             <div class="bookmarks">
-                <h2>Recent Bookmarks</h2>
+                <div class="header">
+                    <h2>Recent Bookmarks</h2>
+                </div>
+                <div class="body">
+                    <template v-for="item in profile?.recent_bookmarks">
+                        <StoryListItem :item="item" />
+                    </template>
+                </div>
+                <div class="footer mt-3 text-end"><a class="">See all bookmarks >></a></div>
             </div>
         </div>
 
     </Container>
 </template>
 <script setup lang="ts">
-import { computed, reactive, watchEffect } from 'vue'
+import { computed, onBeforeMount, reactive, watchEffect } from 'vue'
+import { useProfileStore } from '../stores/profile';
+import { useRoute } from 'vue-router';
+import { storeToRefs } from "pinia";
+
+import StoryListItem from '../components/story/StoryListItem.vue';
+
+import logoDefault from '@/assets/logo.svg'
 import profileExample from '../assets/profile_example.jpg'
 
-const profile = computed(() => { return profileExample })
+// const profile = computed(() => { return profileExample })
+// const logo = computed(() => { return logoDefault })
+
+const { getProfile } = useProfileStore()
+const { profile } = storeToRefs(useProfileStore())
+const route = useRoute()
+
+onBeforeMount(async () => {
+    await getProfile(route.params.id)
+})
 
 </script>
