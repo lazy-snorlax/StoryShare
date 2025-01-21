285c3b8cf84a7d73da20748f81b6a87fc6cac02f
Anthony <blacknebula236@gmail.com>
2025-01-21 15:31:23 +1030

    testing bookmarks with protected stories

--- api/app/Http/Controllers/ProfileController.php
+++ api/app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\ProfileResource;
+use App\Models\Bookmark;
 use App\Models\User\Profile;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,8 @@ class ProfileController extends Controller
         $profile = Profile::where('id', $id)->first();
         $profile->recent_stories = $profile->user()->first()->stories()->canAccess(auth()->user())->limit(4)->orderBy('updated_at', 'desc')->get();
         $profile->recent_bookmarks = $profile->user()->first()->bookmarks()->limit(4)->orderBy('updated_at', 'desc')->get();
+        // dd(Bookmark::query()->where('user_id', $profile->user()->first()->id)->leftJoin('stories', 'bookmarks.story_id', '=', 'stories.id')->toRawSql(););
+        // dd($profile->user()->first()->bookmarks()->limit(4)->orderBy('updated_at', 'desc')->toRawSql());
         return new ProfileResource($profile);
     }
 
