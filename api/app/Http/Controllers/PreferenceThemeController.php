db7d390dc2c2c5cdccb4799117e82476b8e34dc5
Anthony <blacknebula236@gmail.com>
2025-05-11 21:18:54 +0930

    create endpoint to store custom theme

---
+++ api/app/Http/Controllers/PreferenceThemeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\LoggedInResource;
+use App\Models\User\Profile;
+use Illuminate\Http\Request;
+
+class PreferenceThemeController extends Controller
+{
+    public function store(Request $request, $id) {
+        $profile = Profile::where('user_id', $id)->first();
+        // dd($request->input(), $request->input("theme")["dark"]);
+        $preferences = $profile->preferences;
+        $preferences['themes'][$request->input("themeName")] = [
+            "primary" => $request->input("theme")["primary"],
+            "primaryAlt" => $request->input("theme")["primaryAlt"],
+            "light" => $request->input("theme")["light"],
+            "lightAlt" => $request->input("theme")["lightAlt"],
+            "dark" => $request->input("theme")["dark"],
+            "darkAlt" => $request->input("theme")["darkAlt"],
+        ];
+
+        $profile->preferences = $preferences;
+        $profile->save();
+
+        return new LoggedInResource($profile->user()->first());
+    }
+}
