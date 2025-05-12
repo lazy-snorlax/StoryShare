b37fc821f3d7182f934457eef7f4637564b896ec
Anthony <blacknebula236@gmail.com>
2025-05-12 22:42:42 +0930

    load theme from loggedinUser onMounted

--- vue/src/templates/AppTemplate.vue
+++ vue/src/templates/AppTemplate.vue
@@ -21,8 +21,33 @@ import { ModalsContainer, useModal } from 'vue-final-modal'
 import Sidebar from '../components/app/Sidebar.vue'
 import { useAuthStore } from '@/stores/auth'
 import { storeToRefs } from 'pinia'
+import { onMounted, onUnmounted } from 'vue'
+import { useLoggedInUser } from '../composables/use-logged-in-user'
 
+const { loggedInUser } = useLoggedInUser()
 const authStore = useAuthStore()
 const { is_expanded } = storeToRefs(authStore)
+
+onMounted(() => {
+  let className = document.querySelector('#page-content-wrapper')?.className
+  if (loggedInUser?.value.preferences.defaultDark && className == "dark") {
+    console.log(">>> Loading dark theme....", loggedInUser?.value.preferences.defaultDark)
+    loadTheme(loggedInUser?.value.preferences.defaultDark)
+  }
+})
+
+const loadTheme = (themeIdx) => {
+  const theme = loggedInUser?.value.preferences.themes[themeIdx]
+  changeColour('--primary', theme["primary"])
+  changeColour('--primary-alt', theme["primaryAlt"])
+  changeColour('--light', theme["light"])
+  changeColour('--light-alt', theme["lightAlt"])
+  changeColour('--dark', theme["dark"])
+  changeColour('--dark-alt', theme["darkAlt"])
+}
+
+const changeColour = (prop, color) => { 
+  document.documentElement.style.setProperty(prop, color)
+}
 
 </script>
