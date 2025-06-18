bf0b1eb5ae9542a59f65c3059cbbf6a7e33c0622
Anthony <blacknebula236@gmail.com>
2025-06-18 23:16:14 +0930

    create user management controller & resource

---
+++ api/app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+
+class UserController extends Controller
+{
+    public function index(Request $request) {
+        $users = User::get();
+        return UserResource::collection($users->load('role'));
+    }
+
+    public function show(string $user) {
+        return new UserResource(User::find($user)->load('role'));
+    }
+}
