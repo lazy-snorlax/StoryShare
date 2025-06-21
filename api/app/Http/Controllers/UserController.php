6ac248b93f566114a5562ac57de8348afeb2863e
Anthony <blacknebula236@gmail.com>
2025-06-21 23:50:14 +0930

    add update user function

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
+use Silber\Bouncer\Database\Role;
 
 class UserController extends Controller
 {
@@ -13,7 +14,26 @@ class UserController extends Controller
         return UserResource::collection($users->load('role'));
     }
 
-    public function show(string $user) {
-        return new UserResource(User::find($user)->load('role'));
+    public function show(User $user): UserResource {
+        return new UserResource($user->load([
+            'role',
+        ]));
+    }
+
+    public function update(Request $request, User $user) {
+        $user->fill($request->only(['name']));
+
+        // $canMakeRestrictedUpdates = $request->user()->isNot($user);
+        // if ($canMakeRestrictedUpdates && $user->role->id !== $request->input('role_id')) {
+        //     $user->retract($user->role->name);
+        //     $user->assign(Role::find($request->input('role_id')));
+        // }
+
+        // TODO: Verify email if user email is changed
+        // if (($email = $request->input('email')) !== $user->email) {}
+
+        return new UserResource($user->load([
+            'role',
+        ]));
     }
 }
