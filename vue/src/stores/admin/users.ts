e48c08a85c8708c199bc924c7861243102baeec3
Anthony <blacknebula236@gmail.com>
2025-06-22 21:50:52 +0930

    add updateUser + toggleStatus + status_class to user store

--- vue/src/stores/admin/users.ts
+++ vue/src/stores/admin/users.ts
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
 
+const prefix = "/admin/users"
 export const useUserStore = defineStore('user', {
     state: (): UserState => ({
         list: [],
@@ -13,12 +14,21 @@ export const useUserStore = defineStore('user', {
     }),
     actions: {
         async getUsers() {
-            const response = await this.http.get('/admin/users')
+            const response = await this.http.get(`${prefix}`)
             this.list = response.data.data
         },
         async getUser(id: number) {
-            const response = await this.http.get(`/admin/users/${id}`)
+            const response = await this.http.get(`${prefix}/${id}`)
             this.user = response.data.data
+        },
+        async updateUser(id: number, payload) {
+            const response = await this.http.put(`${prefix}/${id}`, payload)
+            this.user = response.data.data
+        },
+        async toggleStatus(id: number) {
+            const response = await this.http.put(`${prefix}/${id}/toggle-status` )
+            this.user.status = response.data.data.status
+            this.user.status_class = response.data.data.status_class
         },
     }
 })
@@ -34,6 +44,7 @@ export type UserResource = {
     email: string,
     role: string,
     status: string,
+    status_class: string,
 }
 
 export type UserForm = {
