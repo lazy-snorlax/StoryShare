8b86f2c9e3a5c6d2894a3938e4be4833682ac8da
Anthony <blacknebula236@gmail.com>
2025-06-22 21:51:13 +0930

    Load css template for admins

--- vue/src/templates/AdminTemplate.vue
+++ vue/src/templates/AdminTemplate.vue
@@ -20,8 +20,20 @@
 import { ModalsContainer, useModal } from 'vue-final-modal'
 import { useAuthStore } from '@/stores/auth'
 import { storeToRefs } from 'pinia'
+import { onMounted } from 'vue'
+import { useLoggedInUser } from '../composables/use-logged-in-user'
+import { changeTheme } from '../composables/change-theme'
 
+const { loggedInUser } = useLoggedInUser()
 const authStore = useAuthStore()
 const { is_expanded } = storeToRefs(authStore)
 
+const { loadTheme } = changeTheme()
+
+onMounted(() => {
+  let className = document.querySelector('#page-content-wrapper')?.className
+  if (loggedInUser?.value.preferences?.defaultDark && className == "dark") {
+    loadTheme(loggedInUser?.value.preferences.defaultDark)
+  }
+})
 </script>
